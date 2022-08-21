886bf99a688e24f0598407116db6b315e8c2965b
jacyluo <894325189@qq.com>
2022-08-21 15:41:57 +0800

    add gorm transaction

--- go.mod
+++ go.mod
@@ -5,12 +5,15 @@ go 1.18
 require (
 	github.com/bytedance/go-tagexpr/v2 v2.9.3
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+	gorm.io/gorm v1.23.8
 )
 
 require (
 	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/henrylee2cn/ameda v1.4.10 // indirect
 	github.com/henrylee2cn/goutil v0.0.0-20210127050712-89660552f6f8 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/nyaruka/phonenumbers v1.0.55 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
---
+++ gorm.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+type SqlRes struct {
+	Sql         string
+	AffectedNum int64
+}
+
+func Transaction(db *gorm.DB, arrSql []SqlRes) error {
+	tx := db.Begin()
+	for i := 0; i < len(arrSql); i++ {
+		if result := tx.Exec(arrSql[i].Sql); result.Error != nil {
+			tx.Rollback()
+			return result.Error
+		} else {
+			if arrSql[i].AffectedNum != 0 {
+				n := arrSql[i].AffectedNum
+				if (n < 0 && result.RowsAffected == 0) || (n > 0 && n != result.RowsAffected) {
+					tx.Rollback()
+					return errors.New(arrSql[i].Sql + " rows affected invalid")
+				}
+			}
+		}
+	}
+	tx.Commit()
+	return nil
+}
